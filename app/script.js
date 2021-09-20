5596ab0085c0667ec93bc444667bf494656d59eb
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-09-20 00:06:38 -0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -4,18 +4,17 @@ const btnConverter =document.querySelector('#btn-converter')
 const updateInfo = document.querySelector('#update-info')
 
 
-async function converter(getUpdateInfo){
+async function converter(getUpdateRate){
     const url = await fetch(`https://v6.exchangerate-api.com/v6/c62a7e04cb7eace0dd080d6b/latest/USD`);
     const response = await url.json();
-    
-    getUpdateInfo(response.time_last_update_utc)
+    getUpdateRate(response.time_last_update_utc)
 }
 
-function getUpdateInfo(time){
-    const date = new Date(time)
-    updateInfo.innerHTML = `Ultima atualização: <br> ${date.toUTCString()}`
+function getUpdateRate(time){
+    const date = new Date(time);
+    updateInfo.innerHTML = `Ultima atualização: <br> ${date.toDateString()}`
 }
 
-converter(getUpdateInfo) 
 
-
+converter(getUpdateRate) 
+converter()
