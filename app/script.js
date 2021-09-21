1de0c19ff5824286622152184018332d57a89091
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-09-21 00:37:46 -0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -1,19 +1,27 @@
+const amountInput = document.querySelector('#amount-input')
 const currencyOne = document.querySelector('#currency-one')
 const currencyTwo = document.querySelector('#currency-two')
 const btnConverter =document.querySelector('#btn-converter')
 const updateInfo = document.querySelector('#update-info')
 
 
-async function converter(getUpdateRate){
+const fetchApiExchange = async () => {
     const url = await fetch(`https://v6.exchangerate-api.com/v6/c62a7e04cb7eace0dd080d6b/latest/USD`);
     const response = await url.json();
-    getUpdateRate(response.time_last_update_utc)
+   
+    return response
 }
 
-function getUpdateRate(time){
-    const date = new Date(time);
-    updateInfo.innerHTML = `Ultima atualização: <br> ${date.toDateString()}`
+const showRates = async () =>{
+    const rateData = await fetchApiExchange();
+
+    const rates = rateData.conversion_rates
+   
+    for(var rate in rates){
+        currencyOne.innerHTML += `<option>${rate}</option>`
+        currencyTwo.innerHTML += `<option>${rate}</option>`
+    }
 }
 
-converter(getUpdateRate) 
-converter()
+fetchApiExchange() 
+showRates()
